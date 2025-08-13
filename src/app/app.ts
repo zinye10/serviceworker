ec6d09f25c477c69642e6a113bdae28cf425a6c1
Zin Ye <octoberboy.1995@gmail.com>
2025-08-14 04:23:40 +0900

    second emmit

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,6 +4,7 @@ import { Post } from './post/post';
 import { HttpClient } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { SwPush } from '@angular/service-worker';
 
 interface post {
   title: string;
@@ -22,11 +23,45 @@ export class App implements OnInit {
   protected title = 'serviceWorker';
   posts: post[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private swPush: SwPush) {}
 
   ngOnInit(): void {
     this.http
       .get<post[]>('https://jsonplaceholder.typicode.com/posts')
       .subscribe((fetchedPosts) => (this.posts = fetchedPosts));
+
+    if ('Notification' in window) {
+      Notification.requestPermission().then((permission) => {
+        if (permission === 'granted') {
+          console.log('Notification permission granted.');
+          this.swPush.notificationClicks.subscribe(
+            ({ action, notification }) => {
+              console.log('Notification clicked:', notification);
+              // Handle navigation or other logic
+            }
+          );
+        } else {
+          console.warn('Notification permission denied.');
+        }
+      });
+    }
+
+    this.swPush.messages.subscribe((message: any) => {
+      console.log('Push message received:', message);
+
+      if (Notification.permission === 'granted') {
+        console.log('hello');
+        navigator.serviceWorker.startMessages();
+        navigator.serviceWorker.getRegistration().then((reg) => {
+          if (reg) {
+            reg.showNotification(message.notification.title, {
+              body: message.notification.body,
+              icon: 'icons/icon-72x72.png',
+              data: message.notification.data,
+            });
+          }
+        });
+      }
+    });
   }
 }
